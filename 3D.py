fc31fcf697bd41a37bb0598200ff2bb63934eab6
PatriksM <pmorevs@gmail.com>
2025-10-20 20:23:50 +0300

    skana walking

--- 3D.py
+++ 3D.py
@@ -20,6 +20,21 @@ sky = Sky()
 
 window.fullscreen = True
 
+walk = Audio(
+    'assets\walking.mp3',
+    loop = False,
+    autoplay = False
+)
+
+def update():
+    walking = held_keys['a'] or held_keys['d'] or held_keys['w'] or held_keys['s']
+    if walking:
+        if not walk.playing: 
+            walk.play()
+    else:
+        if walk.playing:
+            walk.stop()
+
 def input(key):
     if(key == 'q'):
         quit()
